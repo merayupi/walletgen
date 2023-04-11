d30f6d8a359847031a1a758d6e9080eb6b3b8d0d
meracon <bintang555pamungkas@gmail.com>
2023-04-11 19:47:44 +0700

    init file

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 import { randomBytes } from 'node:crypto';
 import { privateToAddress } from 'ethereumjs-util';
-import { createWriteStream } from 'node:fs';
-import 'dotenv/config';
+import { createWriteStream, existsSync } from 'node:fs';
+import readline from 'readline-sync';
 
-let numberToGenerate = process.env.NUMBER_TO_GENERATE;
-let writeStream = createWriteStream('./generated.csv');
+let filename = 'generated.csv';
+let i = 2;
 
-console.log('Generating keys...');
+while(existsSync(filename)) {
+  filename = `generated_${i}.csv`;
+  i++;
+}
+
+let writeStream = createWriteStream(`./${filename}`);
+
+
 
 const main = async () => {
+
+    console.log(`
+    +-----------------------------------------+
+    |
+    | 	EVM Wallet Generator
+    |   by Conny
+    |	
+    +-----------------------------------------+
+            `)
+    const numberToGenerate = readline.question('[?] How Many Wallet: ')
+    console.log('Generating keys...');
+
     writeStream.write('Private Key,Public Key\n');
     for(let i = 0; i < numberToGenerate; i++) {
         let privateKey = randomBytes(32);
         writeStream.write(`${privateKey.toString('hex')},0x${privateToAddress(privateKey).toString('hex')}${i + 1 == numberToGenerate ? '' : '\n'}`);
     }
     writeStream.end();
-    console.log('Wallets generated!');
+    console.log(`Wallets generated and saved in ${filename}!`);
 }
 
 main();
